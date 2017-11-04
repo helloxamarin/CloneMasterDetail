5204f6c145ba16ec2ebe73602914259e387154b1
voyagerwoo <voyager.woo@gmail.com>
2017-11-04 23:37:22 +0900

    메인페이지를 텝 형식의 메인페이지로 변경합니다.

--- CloneMasterDetail/CloneMasterDetail/App.xaml.cs
+++ CloneMasterDetail/CloneMasterDetail/App.xaml.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace CloneMasterDetail
+﻿namespace CloneMasterDetail
 {
-	public partial class App : Application
+	public partial class App
 	{
 		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new CloneMasterDetail.Views.ItemsPage();
+			MainPage = new MainPage();
 		}
 
 		protected override void OnStart ()
