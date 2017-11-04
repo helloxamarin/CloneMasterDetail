e2c7a64ff0446eaaa38dffc9cbaf3e4589245429
voyagerwoo <voyager.woo@gmail.com>
2017-11-05 00:54:25 +0900

    Item 상세 페이지를 추가합니다

---
+++ CloneMasterDetail/CloneMasterDetail/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloneMasterDetail.Models;
+using CloneMasterDetail.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CloneMasterDetail.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemDetailPage : ContentPage
+	{
+	    // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+	    public ItemDetailPage()
+	    {
+	        InitializeComponent();
+
+	        var item = new Item
+	        {
+	            Text = "Example Item",
+	            Description = "This is an example item description for previewer."
+	        };
+
+	        var viewModel = new ItemDetailViewModel(item);
+	        BindingContext = viewModel;
+	    }
+
+	    public ItemDetailPage(ItemDetailViewModel viewModel)
+	    {
+	        InitializeComponent();
+
+	        BindingContext = viewModel;
+	    }
+    }
+}
