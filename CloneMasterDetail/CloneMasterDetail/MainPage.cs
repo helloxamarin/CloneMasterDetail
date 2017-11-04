8e19a0906a1bf0e56adf567cee938622994eaa84
voyagerwoo <voyager.woo@gmail.com>
2017-11-04 23:37:02 +0900

    Tab 형태의 메인 페이지를 추가합니다.

---
+++ CloneMasterDetail/CloneMasterDetail/MainPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CloneMasterDetail.Views;
+using Xamarin.Forms;
+
+namespace CloneMasterDetail
+{
+	public class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+		    Page itemsPage, aboutPage;
+
+		    switch (Device.RuntimePlatform)
+		    {
+                case Device.iOS:
+                    itemsPage = new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse"
+                    };
+
+                    aboutPage = new NavigationPage(new AboutPage())
+                    {
+                        Title = "About"
+                    };
+                    itemsPage.Icon = "tab_feed.png";
+                    aboutPage.Icon = "tab_about.png";
+                    break;
+                default:
+                    itemsPage = new ItemsPage()
+                    {
+                        Title = "Browse"
+                    };
+                    aboutPage = new AboutPage()
+                    {
+                        Title = "About"
+                    };
+                    break;
+		    }
+
+            Children.Add(itemsPage);
+            Children.Add(aboutPage);
+
+		    Title = Children[0].Title;
+		}
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+            Title = CurrentPage?.Title ?? string.Empty;
+        }
+    }
+}
