ba7f707d7e5dfe83c6cc3437729a056d03c18676
voyagerwoo <voyager.woo@gmail.com>
2017-11-04 23:18:06 +0900

    Items 뷰 모델을 커밋합니다

---
+++ CloneMasterDetail/CloneMasterDetail/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using CloneMasterDetail.Models;
+using Xamarin.Forms;
+
+namespace CloneMasterDetail.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public ItemsViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            //TODO MessagingCenter.Subscribe ...
+        }
+
+        private async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
+    }
+}
