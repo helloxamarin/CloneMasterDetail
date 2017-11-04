5133afcfd84187aff02e0699bf84356fea6318fb
voyagerwoo <voyager.woo@gmail.com>
2017-11-05 01:05:21 +0900

    Navigation 을 위해서 페이지를 NavigationPage로 감쌉니다. 그리고 iOS의 경우에는 감싸지 않습니다.

--- CloneMasterDetail/CloneMasterDetail/App.xaml.cs
+++ CloneMasterDetail/CloneMasterDetail/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace CloneMasterDetail
+﻿using CloneMasterDetail.Views;
+using Xamarin.Forms;
+
+namespace CloneMasterDetail
 {
 	public partial class App
 	{
@@ -6,7 +9,10 @@
 		{
 			InitializeComponent();
 
-			MainPage = new Views.MainPage();
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new MainPage();
+		    else
+                MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
