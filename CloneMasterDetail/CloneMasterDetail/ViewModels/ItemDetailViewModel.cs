4692718e87660d38d4864a7b2a3057a4e1a39ea5
voyagerwoo <voyager.woo@gmail.com>
2017-11-04 23:44:54 +0900

    Item 상세 페이지를 위한 ViewModel을 추가합니다.

---
+++ CloneMasterDetail/CloneMasterDetail/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CloneMasterDetail.Models;
+
+namespace CloneMasterDetail.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
