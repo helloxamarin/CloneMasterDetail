6657d5ac29e28c907e025d0e803bcf5a9798fab6
voyagerwoo <voyager.woo@gmail.com>
2017-11-04 23:18:23 +0900

    ItemsPage를 커밋합니다.

---
+++ CloneMasterDetail/CloneMasterDetail/Views/ItemsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CloneMasterDetail.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CloneMasterDetail.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemsPage
+	{
+	    private readonly ItemsViewModel _viewModel;
+
+		public ItemsPage ()
+		{
+			InitializeComponent ();
+
+		    BindingContext = _viewModel = new ItemsViewModel();
+		}
+
+	    private void ItemsListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+            //TODO
+	        throw new NotImplementedException();
+	    }
+
+	    private void AddItem_OnClicked(object sender, EventArgs e)
+	    {
+            //TODO
+	        throw new NotImplementedException();
+	    }
+
+	    protected override void OnAppearing()
+	    {
+	        base.OnAppearing();
+	        if (_viewModel.Items.Count == 0)
+                _viewModel.LoadItemsCommand.Execute(null);
+	    }
+	}
+}
