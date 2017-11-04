70c2a953211ec49a06460a1fd5ab9a9e07889ab6
voyagerwoo <voyager.woo@gmail.com>
2017-11-05 01:37:30 +0900

    Item을 새로 만드는 페이지를 추가합니다.

---
+++ CloneMasterDetail/CloneMasterDetail/Views/NewItemPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloneMasterDetail.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CloneMasterDetail.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewItemPage : ContentPage
+	{
+        public Item Item { get; set; }
+
+		public NewItemPage ()
+		{
+			InitializeComponent ();
+
+		    Item = new Item
+		    {
+		        Text = "Item name",
+		        Description = "This is an item description"
+		    };
+
+		    BindingContext = this;
+		}
+
+	    private async void Save_Clicked(object sender, EventArgs e)
+	    {
+	        MessagingCenter.Send(this, "AddItem", Item);
+	        await Navigation.PopToRootAsync();
+	    }
+	}
+}
