3a58ccaea9f0d7ac7347a3e1c16f75498d4bc03d
voyagerwoo <voyager.woo@gmail.com>
2017-12-16 22:22:28 +0900

    UWP text to speach 기능 추가

---
+++ CloneMasterDetail/CloneMasterDetail.UWP/Services/TextToSpeechImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using CloneMasterDetail.Services;
+using CloneMasterDetail.UWP.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeechImpl))]
+namespace CloneMasterDetail.UWP.Services
+{
+    public class TextToSpeechImpl : ITextToSpeech
+    {
+        public async void Speak(string text)
+        {
+            var mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            var stream = await synth.SynthesizeTextToStreamAsync(text);
+
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+    }
+}
