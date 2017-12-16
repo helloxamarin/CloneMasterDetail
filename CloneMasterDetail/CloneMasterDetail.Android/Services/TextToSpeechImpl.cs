cad75e77b3c237bc531af67441c818700daed5ba
voyager.woo <voyager.woo@gmail.com>
2017-12-16 17:15:33 +0900

    text to speach 서비스 추가

---
+++ CloneMasterDetail/CloneMasterDetail.Android/Services/TextToSpeechImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.Speech.Tts;
+using CloneMasterDetail.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeachImpl))]
+namespace CloneMasterDetail.Droid.Services
+{
+    public class TextToSpeachImpl : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech speaker;
+        string toSpeak;
+
+        public void Speak(string text)
+        {
+            toSpeak = text;
+            if (speaker == null)
+            {
+                speaker = new TextToSpeech(Forms.Context, this);
+            }
+            else
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+
+        public void OnInit(OperationResult status)
+        {
+            if (status.Equals(OperationResult.Success))
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+    }
+}
---
+++ CloneMasterDetail/CloneMasterDetail.iOS/Services/TextToSpeechImpl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AVFoundation;
+using CloneMasterDetail.Services;
+
+[assembly: Dependency(typeof(TextToSpeachImpl))]
+namespace CloneMasterDetail.iOS.Services
+{
+    public class TextToSpeechImpl : ITextToSpeech
+    {
+        public void Speak(string text)
+        {
+            var speechSynthesizer = new AVSpeechSynthesizer();
+            var speechUtterance = new AVSpeechUtterance(text)
+            {
+                Rate = AVSpeechUtterance.MaximumSpeechRate / 4,
+                Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
+                Volume = 0.5f,
+                PitchMultiplier = 1.0f
+            };
+
+            speechSynthesizer.SpeakUtterance(speechUtterance);
+        }
+    }
+}
